63f26b7165ecbbe12c8cdbfd716715fa6c53edc9
Pepealcrack <pepejaladoviedma@gmail.com>
2025-03-04 19:39:39 +0100

    Vistas refactorizadas

--- app/src/main/java/com/dam/ciclismoApp/ui/dashboard/ParticipationsFragment.kt
+++ app/src/main/java/com/dam/ciclismoApp/ui/dashboard/ParticipationsFragment.kt
@@ -1,4 +1,4 @@
-package com.dam.ciclismoApp.ui.home
+package com.dam.ciclismoApp.ui.dashboard
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.dam.ciclismoApp.databinding.FragmentHomeBinding
+import com.dam.ciclismoApp.databinding.FragmentParticipationsBinding
 
-class HomeFragment : Fragment() {
+class ParticipationsFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentParticipationsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val dashboardViewModel =
+            ViewModelProvider(this).get(ParticipationsViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentParticipationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textView
+        dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
