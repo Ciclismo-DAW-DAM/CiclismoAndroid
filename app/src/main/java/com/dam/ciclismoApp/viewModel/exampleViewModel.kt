45754a9ecc040c340d0938621edbb77ca58fa86a
DFran49 <francresmartin@gmail.com>
2025-03-04 18:18:48 +0100

    Implementaciones y Estructura

---
+++ app/src/main/java/com/dam/ciclismoApp/viewModel/exampleViewModel.kt
@@ -0,0 +1,4 @@
+package com.dam.ciclismoApp.viewModel
+
+class exampleViewModel {
+}
--- app/src/main/java/com/dam/ciclismoApp/vista/MainActivity.kt
+++ app/src/main/java/com/dam/ciclismoApp/vista/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.dam.ciclismoApp
+package com.dam.ciclismoApp.vista
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.dam.ciclismoApp.R
 import com.dam.ciclismoApp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
