31ad238ec47af5d951081c1ccecb4947909a6043
Pepealcrack <pepejaladoviedma@gmail.com>
2025-03-05 16:28:02 +0100

    Fuente y fragment participations

--- app/src/main/java/com/dam/ciclismoApp/ui/participations/ParticipationsFragment.kt
+++ app/src/main/java/com/dam/ciclismoApp/ui/participations/ParticipationsFragment.kt
@@ -13,8 +13,6 @@ class ParticipationsFragment : Fragment() {
 
     private var _binding: FragmentParticipationsBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
