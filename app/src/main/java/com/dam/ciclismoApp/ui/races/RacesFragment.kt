671d537617a2768ecdd6d0e7e23fd059c6b5e5f5
Pepealcrack <pepejaladoviedma@gmail.com>
2025-03-05 18:15:55 +0100

    Races creado

--- app/src/main/java/com/dam/ciclismoApp/ui/races/RacesFragment.kt
+++ app/src/main/java/com/dam/ciclismoApp/ui/races/RacesFragment.kt
@@ -13,8 +13,6 @@ class RacesFragment : Fragment() {
 
     private var _binding: FragmentRacesBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -28,10 +26,6 @@ class RacesFragment : Fragment() {
         _binding = FragmentRacesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
