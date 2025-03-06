fea64cb768cb11ccb50d41671d21cf885882a243
DFran49 <francresmartin@gmail.com>
2025-03-06 19:47:14 +0100

    Navigations perfil

--- app/src/main/java/com/dam/ciclismoApp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/dam/ciclismoApp/ui/profile/ProfileFragment.kt
@@ -1,14 +1,18 @@
 package com.dam.ciclismoApp.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.dam.ciclismoApp.databinding.FragmentProfileBinding
+import com.dam.ciclismoApp.ui.AuthActivity
 import com.dam.ciclismoApp.ui.login.LoginViewModel
 import com.dam.ciclismoApp.viewModel.GenericViewModelFactory
 
@@ -27,6 +31,7 @@ class ProfileFragment : Fragment() {
             ViewModelProvider(this).get(ProfileViewModel::class.java)
 
         inicializarBinding(inflater,container)
+        inicializarBotones()
         val root: View = binding.root
 
         val textView: TextView = binding.textView3
@@ -34,6 +39,33 @@ class ProfileFragment : Fragment() {
             textView.text = it
         }
         return root
+    }
+
+    fun inicializarBotones() {
+        val nc = findNavController()
+        val flecha = ProfileFragmentDirections.actionNavigationProfileToUpdateUserFragment()
+        binding.btnUpdate.setOnClickListener {
+            nc.navigate(flecha)
+        }
+
+        inicializarEliminar()
+
+        cerrar(binding.btnCloseSesion)
+    }
+
+    fun inicializarEliminar() {
+        cerrar(binding.btnDeleteUser)
+    }
+
+    fun cerrar(boton: Button) {
+        boton.setOnClickListener {
+            val intent = Intent(requireContext(), AuthActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                putExtra("NAVIGATE_TO_FRAGMENT", "LoginFragment")
+            }
+            startActivity(intent)
+            requireActivity().finish()
+        }
     }
 
     override fun onDestroyView() {
