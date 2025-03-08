a24c3535b20e83a1e1adbe6b549111886888ae61
DFran49 <francresmartin@gmail.com>
2025-03-09 00:47:55 +0100

    Llamada inscribirse

--- app/src/main/java/com/dam/ciclismoApp/models/endpoints/ParticipantsApi.kt
+++ app/src/main/java/com/dam/ciclismoApp/models/endpoints/ParticipantsApi.kt
@@ -1,9 +1,14 @@
 package com.dam.ciclismoApp.models.endpoints
 
 import com.dam.ciclismoApp.models.objects.Participant
+import com.dam.ciclismoApp.models.objects.Registration
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ParticipantsApi {
-    @GET("cycling_participant")
-    suspend fun getParticipantsFromRace():List<Participant>
+    @POST("cycling_participant/new")
+    suspend fun addParticipant(
+        @Body registration: Registration
+    ):Participant
 }
--- app/src/main/java/com/dam/ciclismoApp/models/objects/Participant.kt
+++ app/src/main/java/com/dam/ciclismoApp/models/objects/Participant.kt
@@ -1,6 +1,7 @@
 package com.dam.ciclismoApp.models.objects
 
 import com.squareup.moshi.JsonClass
+import com.squareup.moshi.Moshi
 import java.io.Serializable
 import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
@@ -23,6 +24,20 @@ data class Participant(
         dorsal = 0,
         banned = false
     )
+
+    fun toJson(): String {
+        val moshi = Moshi.Builder().build()
+        val adapter = moshi.adapter(Participant::class.java)
+        val json = adapter.toJson(this)
+        return json
+    }
+
+    fun fromJson(json:String): Participant {
+        val moshi = Moshi.Builder().build()
+        val adapter = moshi.adapter(Participant::class.java)
+        val participante: Participant? = adapter.fromJson(json)
+        return checkNotNull(participante)
+    }
 
     fun getFechaComoOffsetDateTime(): OffsetDateTime =
         OffsetDateTime.parse(time, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
